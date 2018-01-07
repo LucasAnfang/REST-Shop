f773d5f140af51fec4ccb8aef11a46897921fa58
Luke Anfang <lanfang@usc.edu>
2018-01-06 19:17:11 -0500

    Initial commit of bare bones rest api

---
+++ node-rest-shop/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+    'mongodb://luke-anfang:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-3cbfb.mongodb.net:27017,node-rest-shop-shard-00-01-3cbfb.mongodb.net:27017,node-rest-shop-shard-00-02-3cbfb.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin'
+);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Avoid CORS errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// routes that handle requests
+app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+module.exports = app;
